48e206f7cb0aa337663fa09d0e7a2a74638048db
Peter Aspiazu <paspiazusabando@gmail.com>
2022-03-22 15:45:44 -0500

    Actualizando Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,15 +16,13 @@ export const Home = () => {
                 </div>
 
                 <div className="col2-home">
-                    <p>Hola que tal, tengo 25 años estudie una tecnologia en Redes de datos donde vimos muy poco de 
-                        desarrollo web y bases de datos, pero me interese del tema y lo que hice fue por mi propia cuenta
-                        aprender HTML5 y CSS3, y cositas de javascript cuando necesitaba algo como menu hamburguesa, carrusel
-                        de imagenes y validacion basica de formulario del lado del front. Ahora se que se puede hacer mucho
-                        más con Javascript. Tengo mas de 2 años de experiencia en maquetacion con CSS3. Hace aproximadamente 7 meses
-                        empece a aprender Javascript y ahora React tecnología que aún no domino obviamente pero
-                        se hacer unas cuantas cosas como CRUD, rutas protegidas, login y en la pagina de proyectos
-                        pueden ver en que he trabajado, algunos son proyectos hechos por mi mismo sin una guía y otros son de los
-                        que desarrollamos en cursos que he adquirido.
+                    <p>Hola que tal, tengo 1 año desde que empece a aprender sobre desarrollo web y hasta ahora sigo aprendiendo
+                        muchas cosas muy buenas e interesantes como lo son react y firebase, tengo conocimientos de HTML5,
+                        CSS3, Javascript, SASS, Bootstrap, react y firebase. En este último año he aprendido sobre semantica,
+                        diseños adaptables, el DOM, peticiones fetch, rutas con react y rutas protegidas, login y registros 
+                        con firebase, tambien almacenar datos en firestore y subir mis proyectos a github con git y que se
+                        muestren en github pages. Seguire aprendiendo y avanzando para poder conseguir mi primer
+                        empleo como desarrollador.
                     </p>
                 </div>
             </div>
